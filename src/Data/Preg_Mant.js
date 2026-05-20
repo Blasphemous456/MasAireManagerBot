43a8087ad1f611634bb716b8291dd074bc7c357c
Felipe Alvarez Guerra <felipealvaresm@gmail.com>
2026-05-20 18:49:41 -0500

    proyecto terminado

--- src/Data/Preg_Mant.js
+++ src/Data/Preg_Mant.js
@@ -2,53 +2,29 @@
 export const PreguntasMantenimiento = [
   {
     id: 1,
-<<<<<<< HEAD
-    text: "Cual opcion describe mejor tu equipo  ",
-    options: [
-      { value: "a", label: "Funciona bien y solo requiere mantenimiento preventivo" },
-      { value: "b", label: "Ha perdido rendimiento con el tiempo" },
-      { value: "c", label: "Presente ruidos, olores o/y comportamientos irregular" }
-=======
     text: "TU_PREGUNTA_MANT_1_AQUI",
     options: [
       { value: "a", label: "TU_OPCION_MANT_1_A_AQUI" },
       { value: "b", label: "TU_OPCION_MANT_1_B_AQUI" },
       { value: "c", label: "TU_OPCION_MANT_1_C_AQUI" }
->>>>>>> eae431425e9db6790f07faebbe72f260ce7f68fb
     ]
   },
   {
     id: 2,
-<<<<<<< HEAD
-    text: "Cual de estas situaciones se parece mas a tu caso ",
-    options: [
-      { value: "a", label: "Un equipo en uso normal con mantenimineto reciente " },
-      { value: "b", label: "Uno o varios equipos sin manteniminento hace varios meses " },
-      { value: "c", label: "Varios equipos que no han recibido mantenimineto en mucho tiempo " }
-=======
     text: "TU_PREGUNTA_MANT_2_AQUI",
     options: [
       { value: "a", label: "TU_OPCION_MANT_2_A_AQUI" },
       { value: "b", label: "TU_OPCION_MANT_2_B_AQUI" },
       { value: "c", label: "TU_OPCION_MANT_2_C_AQUI" }
->>>>>>> eae431425e9db6790f07faebbe72f260ce7f68fb
     ]
   },
   {
     id: 3,
-<<<<<<< HEAD
-    text: "Como necesitas realizar el servicio",
-    options: [
-      { value: "a", label: "Atencion lo mas pronto posible " },
-      { value: "b", label: "Programarlo en los proximos dias" },
-      { value: "c", label: "Estoy comparando opciones " }
-=======
     text: "TU_PREGUNTA_MANT_3_AQUI",
     options: [
       { value: "a", label: "TU_OPCION_MANT_3_A_AQUI" },
       { value: "b", label: "TU_OPCION_MANT_3_B_AQUI" },
       { value: "c", label: "TU_OPCION_MANT_3_C_AQUI" }
->>>>>>> eae431425e9db6790f07faebbe72f260ce7f68fb
     ]
   }
 ];
--- src/Data/Preg_Repa.js
+++ src/Data/Preg_Repa.js
@@ -2,53 +2,29 @@
 export const PreguntasReparacion = [
   {
     id: 1,
-<<<<<<< HEAD
     text: "Cual opcion describe mejor lo que le pasa a tu equipo",
     options: [
       { value: "a", label: "No enfria correctamente " },
       { value: "b", label: "Presenta fugas, ruidos y/o comportamientos extraños " },
       { value: "c", label: " No enciende o dejo de funcionar " }
-=======
-    text: "TU_PREGUNTA_REP_1_AQUI",
-    options: [
-      { value: "a", label: "TU_OPCION_REP_1_A_AQUI" },
-      { value: "b", label: "TU_OPCION_REP_1_B_AQUI" },
-      { value: "c", label: "TU_OPCION_REP_1_C_AQUI" }
->>>>>>> eae431425e9db6790f07faebbe72f260ce7f68fb
     ]
   },
   {
     id: 2,
-<<<<<<< HEAD
     text: "Como ha evolucionado el problema ",
     options: [
       { value: "a", label: "Aparecio recientemente " },
       { value: "b", label: "Ha estado ocurriendo de forma intermitente " },
       { value: "c", label: "Lleva tiempo y ha empeorado" }
-=======
-    text: "TU_PREGUNTA_REP_2_AQUI",
-    options: [
-      { value: "a", label: "TU_OPCION_REP_2_A_AQUI" },
-      { value: "b", label: "TU_OPCION_REP_2_B_AQUI" },
-      { value: "c", label: "TU_OPCION_REP_2_C_AQUI" }
->>>>>>> eae431425e9db6790f07faebbe72f260ce7f68fb
     ]
   },
   {
     id: 3,
-<<<<<<< HEAD
     text: "Cual opcion describe mejor el funcionamiento actual ",
     options: [
       { value: "a", label: " Funciona correctamente  " },
       { value: "b", label: "Funciona de forma inestable y con fallas " },
       { value: "c", label: " No funciona en absoluto " }
-=======
-    text: "TU_PREGUNTA_REP_3_AQUI",
-    options: [
-      { value: "a", label: "TU_OPCION_REP_3_A_AQUI" },
-      { value: "b", label: "TU_OPCION_REP_3_B_AQUI" },
-      { value: "c", label: "TU_OPCION_REP_3_C_AQUI" }
->>>>>>> eae431425e9db6790f07faebbe72f260ce7f68fb
     ]
   }
 ];
